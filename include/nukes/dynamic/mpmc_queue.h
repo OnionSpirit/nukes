2ab1c8cd2b18f219583b677c03932cf684d4e63e
Ivan Moskalev <MoskalevJobMail@gmail.com>
2026-01-25 02:49:49 +0300

    fixes

--- include/nukes/dynamic/mpmc_queue.h
+++ include/nukes/dynamic/mpmc_queue.h
@@ -189,18 +189,17 @@ pop(dataT& data) noexcept {
         node_t *new_head, *current_head = _head.load(std::memory_order_acquire);
 
         do {
-            // // NOTE: Делаем через goto, потому что continue станет проверять условие
-            // //       с CAS операцией, а нам такого не надо
-            // loop_begin:
+            // NOTE: Делаем через goto, потому что continue станет проверять условие продолжения через CAS,
+            // NOTE: а нам такого не надо, т.к. попортит кеш-линию CAS-перезаписью
+            cxhg_loop:
             if (not current_head) [[unlikely]] return false;
             new_head = reinterpret_cast<node_t *>(current_head->_next.load());
             if (not new_head) [[unlikely]] return false;
-            // if (_head.load(std::memory_order_relaxed) != current_head) [[unlikely]] {
-            //     current_head = _head.load(std::memory_order_relaxed);
-            //     goto loop_begin;
-            // }
-        } while (_head.load(std::memory_order_relaxed) not_eq current_head
-            or not _head.compare_exchange_weak(current_head, new_head,
+            if (_head.load(std::memory_order_relaxed) not_eq current_head) [[unlikely]] {
+                current_head = _head.load(std::memory_order_relaxed);
+                goto cxhg_loop;
+            }
+        } while (not _head.compare_exchange_weak(current_head, new_head,
                                                  std::memory_order_release,
                                                  std::memory_order_relaxed));
 
