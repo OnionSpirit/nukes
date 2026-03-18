ae0510c33e9a9e637cc8cb86953ec9b61048bc34
Ivan Moskalev <MoskalevJobMail@gmail.com>
2026-03-18 18:24:30 +0300

    roaming-mpsc added

---
+++ include/nukes/dynamic/roaming_mpsc_queue.h
@@ -0,0 +1,252 @@
+/**
+ * @file
+ * @details Contains atomic_roaming_mpsc_queue_base declaration
+ */
+#ifndef NUKES_DYNAMIC_ROAMING_MPSC_QUEUE
+#define NUKES_DYNAMIC_ROAMING_MPSC_QUEUE
+
+
+#include <atomic>
+
+#include "constants.h"
+#include "nukes/details/node_types.h"
+#include "nukes/details/misc.h"
+#include "atomic_freelist.h"
+#include "nukes/details/batch.h"
+
+
+namespace nukes::dynamic {
+
+
+/**
+ * @brief An atomic roaming-mpsc queue
+ * @details The atomic roaming-mpsc queue base class, can be used as dyn spsc queue because they have same realization.
+ * <br>Difference between roaming-mpsc and usual mpsc is that @b head pointer of the roaming-mpsc is atomic
+ * to keep it synchronized between caches.
+ * <br>The roaming-mpsc is required when there is a single consumer at the moment of the consumption,
+ * but may have changed between the consumption operation.
+ * @tparam dataT Type that assumed to be used in the queue
+ */
+template <typename dataT>
+struct roaming_mpsc_queue {
+
+    typedef details::nodes::dyn_node<dataT> node_t; ///< Node type declaration
+    typedef atomic_freelist<node_t> mempool_t;      ///< Memory buffer type
+
+private:
+
+    class dyn_roaming_mpsc_iter {
+
+        roaming_mpsc_queue* _queue { nullptr };
+
+    public:
+        explicit dyn_roaming_mpsc_iter(roaming_mpsc_queue* queue)
+            : _queue(queue) {}
+
+        dyn_roaming_mpsc_iter& postfix_increment(details::misc::argument_ref_t<node_t*> ptr) {
+            node_t* new_ptr = ptr->next(), *next_next_ptr = new_ptr->next();
+            if (_queue->recycle_dummy(new_ptr))
+                new_ptr = next_next_ptr;
+            _queue->_mempool.sync(ptr);
+            ptr = new_ptr;
+            return *this;
+        }
+
+        dyn_roaming_mpsc_iter prefix_increment(details::misc::argument_ref_t<node_t*> ptr)  {
+            dyn_roaming_mpsc_iter tmp = *this;
+            node_t* new_ptr = ptr->next(), *next_next_ptr = new_ptr->next();
+            if (_queue->recycle_dummy(new_ptr))
+                new_ptr = next_next_ptr;
+            _queue->_mempool.sync(ptr);
+            ptr = new_ptr;
+            return tmp;
+        }
+    };
+
+    typedef details::batch<node_t, dyn_roaming_mpsc_iter, roaming_mpsc_queue*> batch_t;
+
+protected:
+
+    alignas(64) node_t               _dummy     { };  ///< Dummy node instance
+    node_t* const                    _dummy_ptr { &_dummy };
+    alignas(64) std::atomic<node_t*> _head      { _dummy_ptr };  ///< Head pointer
+    alignas(64) std::atomic<node_t*> _tail      { _dummy_ptr };  ///< Tail pointer
+
+    mempool_t          _mempool   {};  ///< Memory buffer to allocate nodes from
+
+    /**
+     * @details Recycles node, if it's dummy, to the end of the queue
+     * @param node Node instance
+     * @return @b True if node was dummy and recycled to queue tail, @b False otherwise
+     */
+    [[nodiscard]] bool recycle_dummy(details::misc::argument_ref_t<node_t*> node) noexcept;
+
+public:
+
+    roaming_mpsc_queue() noexcept = default;
+
+    /**
+     * @details Atomically pushes element to the queue
+     * @param data Data to be pushed
+     * @return @b True if element successfully pushed,
+     * @b False when run out of capacity
+     */
+    bool push(dataT&& data) noexcept;
+
+    /**
+     * @details Atomically pops an element from the queue to the reference
+     * from function arg, returns the result of operation
+     * @param data Reference to storage of pulled element
+     * @return @b True if element successfully pulled,
+     * @b False when pulling failed or data node sync failed
+     */
+    [[nodiscard]] bool pop(dataT& data) noexcept;
+
+    /**
+     * @details Atomically pushes node instance to the queue (Move Semantics)
+     * @param node Node instance to be pushed
+     */
+    void push_node(details::misc::argument_ref_t<node_t*> node) noexcept;
+
+    /**
+     * @details Atomically releases node to the queue mempool (Move Semantics)
+     * @param node Releasing node
+     */
+    void release_node(details::misc::argument_ref_t<node_t*> node) noexcept;
+
+    /**
+     * @details Atomically pops an node instance from the queue to
+     * the reference from function arg and returns the result of operation
+     * @param node Reference to pulled node storage
+     * @return @b True if node instance successfully pulled,
+     * @b False when pulling failed
+     */
+    [[nodiscard]] node_t* pop_node() noexcept;
+
+    /**
+     * @details Извлекает всю очередь атомарно, в формате листа
+     * @return Объект @b batch содержащий только операции получения итератора,
+     * для прохода по списку
+     */
+    batch_t pop_batch() noexcept {
+        node_t *current_head = _head.load(std::memory_order_relaxed);
+        node_t *current_tail = _tail.load(std::memory_order_relaxed);
+        _head = current_tail;
+        return batch_t { current_head, current_tail, this };
+        // TODO: Weird stuff
+        // node_t *current_head = _head;
+        // node_t *current_tail = _tail.load(std::memory_order_relaxed);
+        // do {
+        //     if (not current_head or not current_tail) [[unlikely]] return batch_t{};
+        // } while (not _tail.compare_exchange_weak(current_tail, current_head,
+        //                                          std::memory_order_release,
+        //                                          std::memory_order_relaxed));
+        //     _head = current_tail;
+        // return batch_t { current_head, current_tail, this };
+    }
+
+    /**
+     * @details Weak operation, can show that empty queue is not empty,
+     * but it will never show that not empty queue is empty
+     * @return @b True when queue is empty (guaranteed), @b False when queue might have elements
+     */
+    [[nodiscard]] bool empty() noexcept;
+
+    roaming_mpsc_queue operator=(roaming_mpsc_queue&) = delete;
+
+    roaming_mpsc_queue& operator=(roaming_mpsc_queue&& q)  noexcept {
+        this->_head = q._head.load(std::memory_order_relaxed);
+        this->_tail = q._tail.load(std::memory_order_relaxed);
+        this->_mempool = std::forward<mempool_t>(q._mempool);
+        return *this;
+    }
+};
+
+} // end namespace nukes::dynamic
+
+
+// ================================ DEFINITIONS ================================
+
+
+#define DYNAMIC_ROAMING_MPSC_QUEUE_MEMBER(member_type)         \
+    template <typename dataT>                          \
+        member_type nukes::dynamic::roaming_mpsc_queue<dataT>::
+
+
+DYNAMIC_ROAMING_MPSC_QUEUE_MEMBER(bool)
+push(dataT&& data) noexcept {
+
+    node_t* new_node { nullptr };
+    if (not _mempool.capture(new_node)) return false;
+    new_node->_data = std::forward<dataT>(data);
+
+    push_node(new_node);
+
+    return true;
+}
+
+
+DYNAMIC_ROAMING_MPSC_QUEUE_MEMBER(bool)
+pop(dataT& data) noexcept {
+
+    auto* released_node = pop_node();
+    if (not released_node) [[unlikely]] return false;
+
+    data = std::forward<dataT>(released_node->_data);
+    return _mempool.sync(released_node);
+}
+
+
+DYNAMIC_ROAMING_MPSC_QUEUE_MEMBER(void)
+release_node(details::misc::argument_ref_t<node_t*> node) noexcept {
+    _mempool.sync(node);
+}
+
+
+DYNAMIC_ROAMING_MPSC_QUEUE_MEMBER(bool)
+recycle_dummy(details::misc::argument_ref_t<node_t*> dummy) noexcept {
+
+    if (dummy == _dummy_ptr) [[unlikely]] {
+        dummy->_next.store(nullptr, std::memory_order_release);
+        node_t *current_tail = _tail.exchange(dummy, std::memory_order_release);
+        current_tail->_next.store(dummy,std::memory_order_release);
+        return true;
+    }
+    return false;
+}
+
+
+DYNAMIC_ROAMING_MPSC_QUEUE_MEMBER(void)
+push_node(details::misc::argument_ref_t<node_t*> node) noexcept {
+
+    node->_next.store(nullptr, std::memory_order_release);
+    node_t* current_tail = _tail.exchange(node, std::memory_order_release);
+    current_tail->_next.store(std::forward<node_t*>(node), std::memory_order_release);
+}
+
+
+DYNAMIC_ROAMING_MPSC_QUEUE_MEMBER(auto)
+pop_node() noexcept -> node_t* {
+    node_t* released_node;
+    do {
+        auto* new_head = reinterpret_cast<node_t *>(_head.load(std::memory_order_acquire)->_next.load(std::memory_order_acquire));
+        if (nullptr == new_head) [[unlikely]] return nullptr;
+        released_node = std::forward<node_t*>(_head.load(std::memory_order_acquire));
+        _head.store(std::forward<node_t*>(new_head), std::memory_order_release);
+    } while (recycle_dummy(released_node));
+
+    return std::forward<node_t*>(released_node);
+}
+
+DYNAMIC_ROAMING_MPSC_QUEUE_MEMBER(bool)
+empty() noexcept {
+
+    if (_head.load(std::memory_order_acquire) == _tail.load(std::memory_order_acquire))
+        return true;
+    return false;
+}
+
+
+
+#undef DYNAMIC_ROAMING_MPSC_QUEUE_MEMBER
+#endif // NUKES_DYNAMIC_ROAMING_MPSC_QUEUE
