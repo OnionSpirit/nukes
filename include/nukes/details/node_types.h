548b96d42e95e503cfc2a081006dbc0f7023e40d
moskalev-i <moskalev-i@protei-lab.ru>
2026-07-01 16:58:28 +0300

    Node casting upgrade

--- include/nukes/details/node_types.h
+++ include/nukes/details/node_types.h
@@ -47,15 +47,15 @@ struct dyn_reg_node {
 };
 
 // NOTE: Casting node from regular queue node to atomic queue
-template <typename dataT>
+template <template <typename> typename node_t, typename dataT>
 auto cast_node(dyn_reg_node<dataT>* node) noexcept {
-    return reinterpret_cast<dyn_node<dataT>*>(node);
+    return reinterpret_cast<node_t<dataT>*>(node);
 }
 
 // NOTE: Casting node from atomic queue to regular queue node
-template <typename dataT>
+template <template <typename> typename node_t, typename dataT>
 auto cast_node(dyn_node<dataT>* node) noexcept {
-    return reinterpret_cast<dyn_reg_node<dataT>*>(node);
+    return reinterpret_cast<node_t<dataT>*>(node);
 }
 
 // NOTE: Тип узла для структур хранения зарезервированной памяти
